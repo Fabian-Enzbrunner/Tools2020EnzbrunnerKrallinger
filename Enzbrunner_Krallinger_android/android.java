9eee1a962059c6c223f067f7ceb31b33dbdc24df
Fabian Enzbrunner <s2010824008@students.fh-hagenberg.at>
2021-05-03 16:03:08 +0200

    First change Master branch

--- Enzbrunner_Krallinger_android/android.java
+++ Enzbrunner_Krallinger_android/android.java
@@ -10,5 +10,8 @@ public void thisIsANewMethod() {
 =======
  //string b = "Hallo";
 >>>>>>> a3d21fc087bd426eeafc5ebb654697d2294ffc2a
+
+//First change from Member2:
+int a = 69 + 420;
 }
 >>>>>>> 03efb08f393cb00eaeb0d3f5f4e7df1322d9a83a
