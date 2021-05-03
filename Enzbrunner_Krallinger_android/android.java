db39728ccee9f1560553b3e326368322eedfd98d
Marcel Krallinger <s2010824017>
2021-05-03 15:21:02 +0200

    Changes from both 2

--- Enzbrunner_Krallinger_android/android.java
+++ Enzbrunner_Krallinger_android/android.java
@@ -2,8 +2,9 @@ public static void () {
 <<<<<<< HEAD
 }
 
-public void thisIsANewMethod() { }
+public void thisIsANewMethod() {
 =======
  int x = 0;
+ double y = b + x;
 }
 >>>>>>> 03efb08f393cb00eaeb0d3f5f4e7df1322d9a83a
